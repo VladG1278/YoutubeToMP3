f0c8729c273c56cfa8534e83ef7d888cbf005e5f
Vlad <vvlad1278@gmail.com>
2023-09-23 13:26:06 -0500

    Downloads Youtube Music Now

--- LinkGetter.py
+++ LinkGetter.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import os
+from pytube import YouTube
 
 # Initiate headless mode to not open a browser
 options = Options()
@@ -15,7 +17,6 @@ driver.get(PlaylistLink)
 elem = driver.find_elements(By.XPATH, "//*[@href]")
 linkList = []
 
-
 # Filters the links for videos and duplicates and adds links to linkList
 num = 0
 for links in elem:
@@ -29,3 +30,27 @@ for links in elem:
         if num == 0:
             linkList.append(temp)
         num = 0
+
+# make a new folder in downloads
+# change this to your download folder path
+path = "C:\\Users\\monke\\Downloads"
+directoryName = input("Enter the name of the playlist: ")
+destination = os.path.join(path, directoryName)
+
+# downloads links
+for links in linkList:
+    yt = YouTube(links)
+
+    # extract only audio
+    video = yt.streams.filter(only_audio=True).first()
+
+    # download the file
+    out_file = video.download(output_path=destination)
+
+    # save the file
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+
+    # result of success
+    print(yt.title + " has been successfully downloaded.")
